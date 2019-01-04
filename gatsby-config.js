ae3a2e9aba56f2db0e05a4213a14c4f11dc137d3
Jake Worth <jake@jakeworth.com>
2019-01-04 14:30:00 -0600

    Activate GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-22571438-2',
       },
     },
     `gatsby-plugin-feed`,
