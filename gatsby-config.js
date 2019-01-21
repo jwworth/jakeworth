68705e9a0fc4ab311ad30c04e62ba1f3ed1d2ebd
Jake Worth <jake@jakeworth.com>
2019-01-21 17:22:59 -0600

    Don't link to assets dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     social: {
       twitter: `jwworth`,
     },
-    twitterCard: `https://www.jakeworth.com/assets/twittercard.jpg`,
+    twitterCard: `https://www.jakeworth.com/twittercard.jpg`,
   },
   plugins: [
     {
