b764b5127e05c20a921c0b3f49b6ab25e7ab357c
Jake Worth <jake@jakeworth.com>
2020-04-10 17:23:34 -0400

    Improve copy

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ const projects = [
   {
     title: 'Today I Learned',
     link: 'https://til.hashrocket.com',
-    stack: 'Elixir, Phoenix, CircleCI, PostgreSQL, Heroku.',
+    stack: 'Elixir, Phoenix, CircleCI, PostgreSQL, Heroku',
     repo: 'https://github.com/hashrocket/tilex/',
     description:
-      'TIL was my apprentice project at Hashrocket. It has grown into a popular developers blog with 2000+ posts, millions of page views per year, the top-ranked answers on Google to many esoteric developer questions. It is also open-source, one of the first such server-rendered Phoenix application repositories available online. I, and members of my team, used TIL to learn Elixir and develop our growing Elixir practice.',
+      "TIL was my apprentice project at Hashrocket. It has grown into a popular developers blog with 2000+ posts, millions of page views per year, and the top-ranked answers on Google to many esoteric developer questions. It's also open-source, one of the first such server-rendered Phoenix application repositories available online. I, and members of my team, used TIL to learn Elixir and develop our growing Elixir practice.",
     purpose:
       "The original purpose of this application was to help me develop my skills as a programmer and consultant while completing the Hashrocket apprenticeship. Today, TIL exists to catalogue the sharing and accumulation of my team's knowledge as it happens day-to-day. Posts have a 200-word limit, and posting is open to any Rocketeer as well as selected friends of Hashrocket. I serve as the core maintainer of the project, collaborating with several interested Hashrocket colleagues and alumni.",
     stackExplanation:
-      'TIL started out was a Ruby on Rails app. In 2016, I initiated an Elixir rewrite because I wanted to have an Elixir project to work on.',
+      'TIL started out was a Ruby on Rails app. In 2016, I initiated an Elixir rewrite because I wanted to learn Elixir and see how Phoenix stacked up against Ruby on Rails.',
     image: 'https://i.imgur.com/6JMNieU.png',
-    lessonsLearned: 'Too many to mention.',
+    lessonsLearned: 'Phoenix stacks up well against Ruby on Rails.',
   },
   {
     title: 'The Bell',
     link: 'https://www.thebellisringing.com/',
-    stack: 'Elixir, Phoenix, WebSockets, PostgreSQL, Gigalixir.',
+    stack: 'Elixir, Phoenix, WebSockets, PostgreSQL, Gigalixir',
     description:
       'The Bell is my COVID-19 social-distancing project— a way to stay connected when you’re apart.',
     purpose:
@@ -30,7 +30,7 @@ const projects = [
   {
     title: 'Game of Life',
     link: 'https://conway.now.sh/',
-    stack: 'React.js, TypeScript, Zeit.',
+    stack: 'React.js, TypeScript, Zeit',
     repo: 'https://github.com/jwworth/conway',
     description:
       "This is my implementation of the classic cellular automaton / interview puzzle, Conway's Game of Life.",
@@ -44,7 +44,7 @@ const projects = [
     title: 'Ceramic Nation',
     link: 'http://novel.herokuapp.com',
     repo: 'https://github.com/jwworth/novel',
-    stack: 'Ruby, Ruby on Rails, CircleCI, PostgreSQL, Heroku.',
+    stack: 'Ruby, Ruby on Rails, CircleCI, PostgreSQL, Heroku',
     description:
       'Ceramic Nation is an auto-generated internet novel, one chapter per day, supported by a Ruby gem I wrote. It produces sometimes enlightening, humorously nonsensical content based on a corpus of classic literature.',
     purpose:
