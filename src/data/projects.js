0a5761d40d779f837aa6c7c31f65c34ef19876d1
Jake Worth <jake@jakeworth.com>
2020-04-29 12:55:39 -0400

    User sees better copy

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     stack: 'Elixir, Phoenix, PostgreSQL, Heroku',
     repo: 'https://github.com/hashrocket/tilex/',
     description:
-      "TIL was my apprentice project at Hashrocket. It has grown into a popular developers blog with 2000+ posts, millions of page views per year, and the top-ranked answers on Google to many an esoteric developer question. It's also open-source, one of the first such Phoenix repositories online. My team and I used TIL to learn Elixir and develop our growing Elixir practice.",
+      "TIL was my apprentice project at Hashrocket. It has grown into a popular developers blog with 2000+ posts, millions of page views per year, and the top-ranked answers on Google to many an esoteric developer question. It's also open-source, one of the first such Phoenix repositories online. My team and I used TIL as a vehicle to learn Elixir and develop our growing Elixir practice.",
     purpose:
       "The purpose of this application was to help me develop my skills as a programmer. Today, TIL exists to catalogue the sharing and accumulation of Hashrocket's knowledge as it happens day-to-day. Posts have a 200-word limit, and posting is open to any Rocketeer as well as selected friends of Hashrocket. I maintain the codebase, collaborating with several interested Hashrocket colleagues and alumni.",
     stackExplanation:
@@ -38,7 +38,7 @@ const projects = [
     purpose:
       'I used this project as a vehicle to better learn React.js and contribute to a growing menagerie of implementations by my team at Hashrocket.',
     stackExplanation:
-      "As a visual concept, I chose React.js with TypeScript. For hosting, Zeit's push-to-deploy and global CDN have been outstanding.",
+      "I knew this project would be highly visual, so I chose React.js, and hard to test, so I added TypeScript. For hosting, Zeit's push-to-deploy and global CDN have been outstanding.",
     image: 'https://i.imgur.com/gsfpbsx.gif',
     lessonsLearned:
       "React is a fantastic platform for visual programs. TypeScript helps me feel more confident in code I didn't test-drive.",
@@ -49,7 +49,7 @@ const projects = [
     repo: 'https://github.com/jwworth/novel',
     stack: 'Ruby, Ruby on Rails, PostgreSQL, Heroku',
     description:
-      'Ceramic Nation is an auto-generated internet novel, one chapter per day, supported by a Ruby gem I wrote called Remarkovable. It produces sometimes enlightening, nonsensical content based on a corpus of classic literature.',
+      'Ceramic Nation is an auto-generated internet novel, one chapter per day, supported by a Ruby gem I wrote called Remarkovable. It produces nonsensical, occasionally interesting writing based on a corpus of classic literature.',
     purpose:
       "Hashrocket Chicago had a brief obsession with Markov chains: programs that produce content based on a statistical analysis of past content. Imagine listening to a person talk for an hour and then guessing what they'll say next; that's Markov. As a new developer on the team, I embraced FOMO and wrote my own implementation.",
     stackExplanation:
