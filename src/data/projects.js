6677cc6affc97e607e57dbe07b404e40fa147e4a
Jake Worth <jake@jakeworth.com>
2021-03-07 13:00:11 -0500

    Simplify

--- src/data/projects.js
+++ src/data/projects.js
@@ -23,7 +23,7 @@ const projects = [
     description:
       'Here\'s the pitch: "Do you want to really (really!) learn React, while staying current on this exploding ecosystem? Subscribe to React Explained, a bi-weekly newsletter of amazing, curated React news and resources, simply explained. It’s free! Subscribe now."',
     purpose:
-      "I found React difficult to learn, because it's built on a couple of revolutionary ideas. In my role as a mentor at Code Platoon, I’ve watched others struggle to learn React, again and again. In 2020, I did surveyed the landscape and concluded that the existing newsletters in the React space aren't geared for beginners. I want to help somebody learning React today– a person who is overwhelmed by the rapid growth of the ecosystem and tons of shared context in the community.",
+      "In 2020, I surveyed the technical newletter space and concluded that the existing React newsletters aren't geared for beginners. I want to help somebody learning React today– a person who is overwhelmed by the rapid growth of the ecosystem and tons of shared context in the community.",
     image: 'https://imgur.com/vu5MAzm.png',
   },
   {
